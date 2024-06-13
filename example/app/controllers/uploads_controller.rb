f5de13a2d12ca13f3064b703b6bda23707ebccc4
Karol Bajko <karol.bajko@gainsight.com>
2024-06-13 15:51:20 +0200

    Fix brakeman offence
    [PLATFORM-1234]

--- example/app/controllers/uploads_controller.rb
+++ example/app/controllers/uploads_controller.rb
@@ -1,5 +1,8 @@
 class UploadsController < ApplicationController
-  http_basic_authenticate_with name: 'user', password: 'password'
+  http_basic_authenticate_with(
+    name: ENV.fetch("HTTP_BASIC_USER", "user"),
+    password: ENV.fetch("HTTP_BASIC_PASSWORD", "password")
+  )
 
   def create
     head 201
